0a233463cbc2929ead8eed7ecfca72c0f39db1a5
Paymon <paymon@geeks.ltd.uk>
2017-12-12 12:52:07 +0000

    Select Tag Helper

--- Olive.Mvc/TagHelpers/SelectTagHelper.cs
+++ Olive.Mvc/TagHelpers/SelectTagHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Olive.Entities;
@@ -13,10 +15,10 @@ namespace Olive.Mvc
 
         public override int Order => 0;
 
+        Type ModelType => For?.Model?.GetType();
+
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            var isEntity = For?.Model?.GetType()?.IsA<IEntity>() ?? false;
-
             // It should clear the original tag helper effects because it will call the base method again.
             output.PreContent.Clear();
             output.Content.Clear();
@@ -24,17 +26,17 @@ namespace Olive.Mvc
 
             await base.ProcessAsync(context, output);
 
-            if (isEntity)
+            if (ModelType?.IsA<IEntity>() ?? false)
             {
                 var postContent = output.PostContent.GetContent();
-
                 var stringId = ((IEntity)For.Model).GetId().ToString();
-
                 postContent = postContent.Replace($"\"{stringId}\"", $"\"{stringId}\" selected=\"selected\"");
-
                 output.PostContent.Clear();
-
                 output.PostContent.SetHtmlContent(postContent);
+            }
+            else if (ModelType?.IsGenericOf(typeof(IEnumerable<>), typeof(IEntity)) ?? false)
+            {
+
             }
         }
     }
