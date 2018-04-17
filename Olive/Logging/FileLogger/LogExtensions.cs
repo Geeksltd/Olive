9651d5eddaf3a79072a3b49d82255a87c77abd88
Paymon <paymon@geeks.ltd.uk>
2018-04-17 13:32:29 +0100

    no message

--- Olive/Logging/FileLogger/LogExtensions.cs
+++ Olive/Logging/FileLogger/LogExtensions.cs
@@ -6,18 +6,10 @@ namespace Olive
 {
     public static class LogExtensions
     {
-        public static ILoggingBuilder AddFile(this ILoggingBuilder @this, string filePrefix = "log-")
-        {
-            @this.AddFile(options => options.FilePrefix = filePrefix);
-            return @this;
-        }
-
         public static ILoggingBuilder AddFile(this ILoggingBuilder @this, Action<FileLoggerOptions> configure = null)
         {
             @this.Services.AddSingleton<ILoggerProvider, FileLoggerProvider>();
-
             if (configure != null) @this.Services.Configure(configure);
-
             return @this;
         }
     }
