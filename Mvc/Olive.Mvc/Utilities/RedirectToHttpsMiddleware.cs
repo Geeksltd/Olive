df4a29feff0743b3237bbf1e68bb8103ec984847
G\Matt <matt@geeks.ltd.uk>
2018-04-18 17:50:12 +0100

    Http to https

---
+++ Mvc/Olive.Mvc/Utilities/RedirectToHttpsMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Olive.Mvc
+{
+    public class RedirectToHttpsMiddleware
+    {
+        private readonly RequestDelegate Next;
+        public RedirectToHttpsMiddleware(RequestDelegate next)
+        {
+            Next = next;
+        }
+        public System.Threading.Tasks.Task Invoke(HttpContext context)
+        {
+            if (!(context.Request.IsHttps || context.Request.Headers.ContainsKey("X-ARR-SSL")))
+            {
+                var request = context.Request;
+                var redirectUrl = $"https://{request.Host}{request.PathBase}{request.Path}{request.QueryString}";
+                context.Response.Redirect(redirectUrl, true);
+            }
+
+            return Next(context);
+        }
+    }
+
+    public static class RedirectToHttpsArrMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRedirectToHttps(
+    this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RedirectToHttpsMiddleware>();
+        }
+    }
+}
