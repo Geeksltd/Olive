0ed0553773d459bd4e1b1b0331b72495fe857fcd
G\Matt <matt@geeks.ltd.uk>
2018-06-18 10:43:50 +0100

    no message

---
+++ Mvc/Olive.Security.Aws/DataKeyService.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Amazon.KeyManagementService;
+using Amazon.KeyManagementService.Model;
+
+namespace Olive.Security.Aws.Services
+{
+    class DataKeyService
+    {
+        static readonly string MasterKeyArn = Config.GetOrThrow("Aws:Kms:MasterKeyArn");
+
+        internal static async Task<Key> GenerateKey()
+        {
+            using (var kms = new AmazonKeyManagementServiceClient())
+            {
+                var dataKey = await kms.GenerateDataKeyAsync(new GenerateDataKeyRequest
+                {
+                    KeyId = MasterKeyArn,
+                    KeySpec = DataKeySpec.AES_256
+                });
+
+                return new Key
+                {
+                    Cipher = dataKey.CiphertextBlob.ReadAllBytes(),
+                    Plain = dataKey.Plaintext.ReadAllBytes()
+                };
+            }
+        }
+
+
+        internal static async Task<byte[]> GetPlainKey(byte[] cipher)
+        {
+            using (var kms = new AmazonKeyManagementServiceClient())
+            {
+                var decryptedData = await kms.DecryptAsync(new DecryptRequest
+                {
+                    CiphertextBlob = cipher.AsStream()
+                });
+
+                return decryptedData.Plaintext.ReadAllBytes();
+            }
+        }
+
+        internal class Key
+        {
+            public byte[] Cipher { get; set; }
+            public byte[] Plain { get; set; }
+        }
+    }
+}
