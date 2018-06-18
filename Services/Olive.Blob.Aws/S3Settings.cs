79479a1d0b4a43397a2b43c73c819a634ae3aada
Paymon <paymon@geeks.ltd.uk>
2018-06-18 14:17:09 +0100

    no message

---
+++ Services/Olive.Blob.Aws/S3Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Olive.BlobAws
+{
+    public class S3Settings
+    {
+        public string AccessKey { get; set; }
+        public string SecretKey { get; set; }
+        public string Region { get; set; }
+
+        public Amazon.RegionEndpoint RegionEndPoint => Amazon.RegionEndpoint.GetBySystemName(Region);
+
+        public static S3Settings Current = Config.Bind<S3Settings>("Aws:S3");
+    }
+}
