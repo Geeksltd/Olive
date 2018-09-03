d275b370ec1a09cda20058da7ba04a505068094d
Paymon <paymon@geeks.ltd.uk>
2018-09-03 18:40:59 +0100

    no message

--- Mvc/Olive.Mvc/Utilities/Controller.File.cs
+++ Mvc/Olive.Mvc/Utilities/Controller.File.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 using Olive.Entities;
 using System.IO;
 using System.Threading.Tasks;
@@ -11,11 +12,21 @@ namespace Olive.Mvc
         /// Gets a FilePathResult based on the file's path. It sets the mime type based on the file's extension.
         /// </summary>
         /// <param name="downloadFileName">If specified, the browser will not try to process the file directly (such as PDF files) and instead always opens the file download dialogue.</param>
-        protected async Task<ActionResult> File(Blob file, string downloadFileName = null)
-            => File(await file.GetFileDataAsync(), file.GetMimeType(), downloadFileName.Or(file.FileName));
+        protected async Task<ActionResult> File(Blob file,
+            string downloadFileName = null, CacheControlHeaderValue cacheControl = null)
+        {
+            if (cacheControl != null)
+            {
+                Response.Headers.Remove(HeaderNames.Pragma);
+                Response.Headers.Remove(HeaderNames.CacheControl);
+                Response.Headers.Add(HeaderNames.CacheControl, cacheControl.ToString());
+            }
 
-        protected JsonResult NonobstructiveFile(byte[] data, string filename) =>
-            AddAction(TempFileService.CreateDownloadAction(data, filename));
+            return File(await file.GetFileDataAsync(), file.GetMimeType(), downloadFileName.Or(file.FileName));
+        }
+
+        protected JsonResult NonobstructiveFile(byte[] data, string filename)
+          => AddAction(TempFileService.CreateDownloadAction(data, filename));
 
         protected async Task<JsonResult> NonobstructiveFile(Blob file, string downloadFileName = null) =>
             NonobstructiveFile(await file.GetFileDataAsync(), downloadFileName.Or(file.FileName));
