f7e5fbd52070da7d3874921f47b0e45fef53e021
Paymon <paymon@geeks.ltd.uk>
2018-10-25 11:54:16 +0100

    no message

---
+++ Utils/MSharp.Build/Tools/NetCoreGlobalTool.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace MSharp.Build.Tools
+{
+    abstract class NetCoreGlobalTool : BuildTool
+    {
+        public override FileInfo ExpectedPath => WindowsCommand.LocalNuget($"tools\\{Name}.exe").AsFile();
+
+        protected override FileInfo Installer => WindowsCommand.DotNet;
+
+        protected override string InstallCommand => $"tool install -g {Name}";
+    }
+}
