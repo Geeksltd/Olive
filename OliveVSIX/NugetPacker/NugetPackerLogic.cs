3db3d669fdabf630d0dd4562da0c0e16f7e0d913
MRP <ipazooki@gmail.com>
2018-11-06 14:05:57 +0330

    Fix nuget push package.

--- OliveVSIX/NugetPacker/NugetPackerLogic.cs
+++ OliveVSIX/NugetPacker/NugetPackerLogic.cs
@@ -95,22 +95,11 @@ namespace OliveVSIX.NugetPacker
 
             if (TryPackDotnet(projectAddress, out string packingMessageDotnet))
             {
-                if (!TryPushDotnet(packageFilename, out string pushingMessage))
+                if (!TryPush(packageFilename, out string pushingMessage))
                     InvokeException(new Exception(pushingMessage));
             }
             else
                 InvokeException(new Exception(packingMessageDotnet));
-        }
-
-        static bool TryPushDotnet(string packageFilename, out string message)
-        {
-            if (!ExecuteNuget($"nuget push \"{NugetPackagesFolder}\\{packageFilename}\" -k {ApiKey} -s https://www.nuget.org/api/v2/package", out message, "dotnet"))
-                return false;
-
-            if (!ExecuteNuget($"nuget push \"{NugetPackagesFolder}\\{packageFilename}\" -k thisIsMyApiKey -s http://nuget.geeksms.uat.co/nuget", out message, "dotnet"))
-                return false;
-
-            return true;
         }
 
         static bool TryPush(string packageFilename, out string message)
@@ -127,8 +116,11 @@ namespace OliveVSIX.NugetPacker
         static bool TryPackNuget(string nuspecAddress, out string message) =>
             ExecuteNuget($"pack \"{nuspecAddress}\" -OutputDirectory \"{NugetPackagesFolder}\"", out message, NugetExe);
 
-        static bool TryPackDotnet(string projectAddress, out string message) =>
-            ExecuteNuget($"pack \"{projectAddress}\" -o \"{NugetPackagesFolder}\"", out message, "dotnet");
+        static bool TryPackDotnet(string projectAddress, out string message)
+        {
+            ExecuteNuget($"build \"{projectAddress}\"", out message, "dotnet");
+            return ExecuteNuget($"pack \"{projectAddress}\" -o \"{NugetPackagesFolder}\"", out message, "dotnet");
+        }
 
         static bool ExecuteNuget(string arguments, out string message, string processStart)
         {
