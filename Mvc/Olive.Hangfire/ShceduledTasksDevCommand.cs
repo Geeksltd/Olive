a7061c404ba4480739f50756ec8ed6ab4dbd1ce4
hamid_mayeli_geeksltd <hamid.mayeli@geeks.ltd.uk>
2018-11-13 15:58:35 +0000

    Complete the ShceduledTasksDevCommand.

--- Mvc/Olive.Hangfire/ShceduledTasksDevCommand.cs
+++ Mvc/Olive.Hangfire/ShceduledTasksDevCommand.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Linq;
+using Hangfire;
+using Hangfire.Storage;
+using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 
 namespace Olive.Hangfire
@@ -17,13 +20,37 @@ namespace Olive.Hangfire
 
         public async Task<string> Run()
         {
-            return @"<html>
+            return $@"<html>
                         <body>
-                           <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/ jquery.min.js'>
-                           </script>
-                           TODO: Add the hangfire tasks information here.
+                            <style>
+                                *{{color: #545454;}}
+                                tr:nth-child(even), thead{{background-color: #f2f2f2;}}
+                                td, th{{padding: 3px 5px;}}
+                            </style>
+                            <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/ jquery.min.js'>
+                            </script>
+                            <table>
+                                <thead><tr>
+                                    <th>Id</th>
+                                    <th>Last execution</th>
+                                    <th>Last execution status</th>
+                                    <th>Next execution</th>
+                                </tr></thead>
+                                {JobStorage.Current.GetConnection().GetRecurringJobs().Select(j => ToMarkup(j)).ToString("")}
+                            </table>
                         </body>
                      </html>";
+        }
+
+        private string ToMarkup(RecurringJobDto job)
+        {
+            var dateFormat = "yyyy/MMM/dd H:mm:ss zzz";
+            string td(string value) => $"<td>{value}</td>";
+
+            return $"<tr>{td(job.Id)}" +
+                td(job.LastExecution.ToString(dateFormat)) +
+                td(job.LastJobState) +
+                $"{td(job.NextExecution.ToString(dateFormat))}</tr>";
         }
     }
 }
