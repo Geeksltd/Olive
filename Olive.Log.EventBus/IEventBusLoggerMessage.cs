f23760331ef9d27288d2e1d38b7bcb507d1acc74
Soroush Asadzadeh <s.asadzadeh.bitbucket@gmail.com>
2018-12-13 19:56:26 +0330

    -

--- Olive.Log.EventBus/IEventBusLoggerMessage.cs
+++ Olive.Log.EventBus/IEventBusLoggerMessage.cs
@@ -1,21 +1,21 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace Olive.Log.EventBus
-//{
-//    public class EventBusLoggerMessage : IEventBusMessage
-//    {
-//        /// <summary>
-//        /// list of Log Messages which publish in an interval
-//        /// </summary>
-//        public IEnumerable<LogMessage> LogMessages { set; get; }
+namespace Olive.Log.EventBus
+{
+    public class EventBusLoggerMessage : IEventBusMessage
+    {
+        /// <summary>
+        /// list of Log Messages which publish in an interval
+        /// </summary>
+        public IEnumerable<LogMessage> LogMessages { set; get; }
 
-//        /// <summary>
-//        /// The DateTime when this message published
-//        /// </summary>
-//        public DateTime PublishDateTime { set; get; }
+        /// <summary>
+        /// The DateTime when this message published
+        /// </summary>
+        public DateTime PublishDateTime { set; get; }
 
-//        public string DeduplicationId => Guid.NewGuid().ToString();
-//    }
-//}
+        public string DeduplicationId => Guid.NewGuid().ToString();
+    }
+}
