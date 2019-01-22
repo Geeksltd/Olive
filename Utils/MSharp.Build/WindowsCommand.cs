bee6d37003112e1c28b91b10637177cb341ffba6
G\Matt <matt@geeks.ltd.uk>
2019-01-22 09:48:46 +0000

    WHERE.exe is being looked for in System32\\System32\\WHERE.exe instead of System32\\WHERE.exe

--- Utils/MSharp.Build/WindowsCommand.cs
+++ Utils/MSharp.Build/WindowsCommand.cs
@@ -8,7 +8,7 @@ namespace MSharp.Build
     {
         public static FileInfo Yarn, Chocolaty, NodeJs, TypeScript, WebPack, Bower, DotNet;
 
-        public static FileInfo Where => System32("System32\\WHERE.exe").ExistsOrThrow();
+        public static FileInfo Where => System32("WHERE.exe").ExistsOrThrow();
 
         public static FileInfo Powershell => System32("windowspowershell\\v1.0\\powershell.exe");
 
