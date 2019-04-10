4e4f944f0675943fd2ae47ada9019d98f37c15f8
HamidTheGeek <hamid.mayeli@yahoo.com>
2019-04-10 10:53:11 +0100

    Fix `AddPropertyEncryption`

--- Olive.Entities.Data/PropertyEncryption/PropertyEncryptionExtensions.cs
+++ Olive.Entities.Data/PropertyEncryption/PropertyEncryptionExtensions.cs
@@ -7,6 +7,16 @@
     {
         public static void AddPropertyEncryption(this IServiceCollection @this, Assembly domainAssembly)
         {
+            @this.AddSingleton<IEntitySavingInterceptor, EncryptedEntitySavingInterceptor>();
+            @this.AddSingleton<IEntityLoadedInterceptor, EncryptedEntityLoadedInterceptor>();
+            EncryptedEntityInterceptor.Initialize(domainAssembly);
+        }
+        public static void AddPropertyEncryption<SavingInterceptor, LoadedInterceptor>(this IServiceCollection @this, Assembly domainAssembly)
+            where SavingInterceptor : class, IEntitySavingInterceptor
+            where LoadedInterceptor : class, IEntityLoadedInterceptor
+        {
+            @this.AddSingleton<IEntitySavingInterceptor, SavingInterceptor>();
+            @this.AddSingleton<IEntityLoadedInterceptor, LoadedInterceptor>();
             EncryptedEntityInterceptor.Initialize(domainAssembly);
         }
     }
