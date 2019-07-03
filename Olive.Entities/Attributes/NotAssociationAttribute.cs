e8e685cf51284a89751b64f79d5a86f105d96f07
HamidTheGeek <hamid.mayeli@yahoo.com>
2019-07-03 17:55:22 +0100

    Add NotAssociationAttribute

---
+++ Olive.Entities/Attributes/NotAssociationAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Olive.Entities
+{
+    /// <summary>
+    /// When applied to a property, indicates that such property is not an association. 
+    /// It uses when you need a property ending with `Id` but it is not an entity.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class NotAssociationAttribute : Attribute
+    {
+        /// <summary>
+        /// Determines if a given property is mark by this attribute.
+        /// </summary>
+        public static bool Marked(PropertyInfo property)
+        {
+            return property.GetCustomAttribute<NotAssociationAttribute>(inherit: true) != null;
+        }
+    }
+}
