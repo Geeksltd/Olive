ba1785666579fe8bb8d5a0ee34bd0e80e42753fb
Paymon <paymon@geeks.ltd.uk>
2019-07-24 15:42:33 +0100

    no message

--- Olive.Aws/RuntimeIdentity.cs
+++ Olive.Aws/RuntimeIdentity.cs
@@ -10,7 +10,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Temp
+namespace Olive.Aws
 {
     /// <summary>
     /// Specifies the current application's runtime IAM identity.
