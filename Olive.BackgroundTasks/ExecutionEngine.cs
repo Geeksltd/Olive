605b563e40de2642f4014cd58abc5cc3b7d3734c
G\Matt <matt@geeks.ltd.uk>
2019-12-09 18:07:24 +0000

    Olive.DistributedBackgroundTasks

---
+++ Olive.BackgroundTasks/ExecutionEngine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Olive.DistributedBackgroundTasks
+{
+    class ExecutionEngine
+    {
+        internal static Guid Id { get; } = Guid.NewGuid();
+        internal static Task RunAll(IEnumerable<IBackgourndTask> tasks) =>
+            Task.WhenAll(tasks.Select(t => TaskExecution.Run(t)));
+    }
+}
---
+++ Olive.BackgroundTasks/Scheduler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olive.DistributedBackgroundTasks
+{
+    class Scheduler
+    {
+        internal static Task<IEnumerable<IBackgourndTask>> GetTasksToRun() =>
+            Context.Current.Database().GetList<IBackgourndTask>().WhereAsync(i => i.TryPick());
+    }
+}
