5cd9f6faaac0d22874185886060aadee40a2fb5f
HamidTheGeek <hamid.mayeli@yahoo.com>
2020-04-16 15:28:58 +0100

    Add ItemGroup to IAuditEvent

--- Olive.Audit/IAudit.cs
+++ Olive.Audit/IAudit.cs
@@ -6,9 +6,9 @@ namespace Olive.Audit
     public interface IAudit
     {
         Task Log(IAuditEvent auditEvent);
-        Task Log(string @event, string data);
-        Task Log(string @event, string data, IEntity item);
-        Task Log(string @event, string data, IEntity item, string userId, string userIp);
+        Task Log(string @event, string data, string group = null);
+        Task Log(string @event, string data, IEntity item, string group = null);
+        Task Log(string @event, string data, IEntity item, string userId, string userIp, string group = null);
         Task LogDelete(IEntity entity);
         Task LogInsert(IEntity entity);
         Task LogUpdate(IEntity entity);
