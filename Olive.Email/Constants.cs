6cf51ffdfaefb9200700bae173fd92abd17ef23c
HamidTheGeek <hamid.mayeli@yahoo.com>
2020-05-19 17:08:03 +0100

    Add email item id to the email headers.

---
+++ Olive.Email/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Olive.Email
+{
+    public class Constants
+    {
+        public const string EMAIL_MESSAGE_ID_HEADER_KEY = "X-Olive-Email-Item-Id";
+    }
+}
