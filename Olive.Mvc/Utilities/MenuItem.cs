5b5d0c9b0899463c0c9aabdd13de9c1471fa6365
HamidTheGeek <hamid.mayeli@yahoo.com>
2020-07-23 15:50:11 +0100

    Fix the MatchesCurrentUrl

--- Olive.Mvc/Utilities/MenuItem.cs
+++ Olive.Mvc/Utilities/MenuItem.cs
@@ -15,7 +15,7 @@
         public bool MatchesCurrentUrl()
         {
             var currentUrl = Context.Current.Request().ToPathAndQuery().UrlDecode();
-            return currentUrl.StartsWith(Url.OrEmpty().RemoveFrom("?"), caseSensitive: false);
+            return currentUrl.StartsWith(Url.OrEmpty(), caseSensitive: false);
         }
     }
 }
