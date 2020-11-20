8355a4553b60f9f0b1941065d9876c1b5a1b0632
Matt <matt@geeks.ltd.uk>
2020-11-20 17:14:29 +0000

    Olive.SMS.TextMagic.1.0.1

---
+++ Olive.SMS.TextMagic/IServiceCollectionExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Olive.SMS;
+
+namespace Olive.Aws.Ses
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection AddAwsSesProvider(this IServiceCollection @this)
+        {
+            return @this.AddTransient<ISmsDispatcher, Olive.SMS.TextMagic.SmsDispatcher>();
+        }
+    }
+}
---
+++ Olive.SMS.TextMagic/SmsDispatcher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using TextmagicRest;
+using Olive;
+
+namespace Olive.SMS.TextMagic
+{
+    public class SmsDispatcher : Olive.SMS.ISmsDispatcher
+    {
+
+        public Task Dispatch(ISmsMessage sms)
+        {
+            var client = new Client(Config.Get("Sms:TextMagic:Username"), Config.Get("Sms:TextMagic:Key"));
+            var result = client.SendMessage(sms.Text, sms.To);
+
+            if (result.Success)
+                throw new Exception("Failed to send message because : " + result.ClientException.Message);
+
+            return Task.CompletedTask;
+        }
+
+    }
+}
