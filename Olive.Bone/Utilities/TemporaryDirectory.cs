fe368a7ca88cfc0ec3030a51df695c71e8c661a8
Mohsen Seifi <mohsens2@outlook.com>
2021-02-13 19:33:12 +0330

    Added Temp Directory

---
+++ Olive.Bone/Utilities/TemporaryDirectory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace Olive.Utilities
+{
+    public class TemporaryDirectory : IDisposable
+    {
+        static readonly string TemporaryFileFolder = Path.GetTempPath();
+
+        Guid ID;
+
+        public string Extension { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of temporary file. The file will have "dat" extension by default.
+        /// </summary>
+        public TemporaryDirectory()
+        {
+
+        }
+
+        /// <summary>
+        /// Gets or sets the FilePath of this TemporaryFile.
+        /// </summary>
+        public string FilePath
+        {
+            get
+            {
+                var folder = TemporaryFileFolder.AsDirectory().CreateSubdirectory(ID.ToString());
+                return folder.FullName;
+            }
+        }
+
+        
+
+        /// <summary>
+        /// Disposes this instance of temporary file and deletes the file if provided
+        /// </summary>
+        public void Dispose()
+        {
+            try
+            {
+                if (FilePath.AsDirectory().Exists()) Directory.Delete(FilePath,true);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex, "Can not dispose temporary directory.");
+            }
+        }
+    }
+}
