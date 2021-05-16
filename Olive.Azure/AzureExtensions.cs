9ab11737a765f087435bbe8000eb3ef294fbefa4
Matt <matt@geeks.ltd.uk>
2021-05-16 13:03:27 +0100

    update

--- Olive.Azure/AzureExtensions.cs
+++ Olive.Azure/AzureExtensions.cs
@@ -7,11 +7,16 @@ namespace Olive
 {
     public static class AzureExtensions
     {
-        public static void LoadAzureDevIdentity(this IConfiguration @this, bool loadSecrets = false)
-        {
-            throw new NotImplementedException();
-        }
+        //public static void LoadAzureDevIdentity(this IConfiguration @this, bool loadSecrets = false)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
+        /// <summary>
+        /// Loads secret information from Azure App Configuration. 
+        /// Required settings : Azure:Secrets:Key, Azure:Secrets:Endpoint
+        /// </summary>
+        /// <param name="this"></param>
         public static void LoadAzureSecrets(this IConfiguration @this) => new Secrets(@this).Load();
     }
 
