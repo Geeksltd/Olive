e101f4e0ab8ca7e0a7213b9c65497ff83aaeea69
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2021-05-19 16:07:03 +0430

    Update

--- Olive/-Extensions/Linq.cs
+++ Olive/-Extensions/Linq.cs
@@ -380,6 +380,12 @@ namespace Olive
 
 		/// <summary>
 		/// Performs an action for all items within the list.
+		/// </summary>        
+		public static Task DoAsync<T>(this IEnumerable<T> @this, Func<T, Task> action)
+			=> @this.DoAsync((x, i) => action(x));
+
+		/// <summary>
+		/// Performs an action for all items within the list.
 		/// It will provide the index of the item in the list to the action handler as well.
 		/// </summary>        
 		public static async Task DoAsync<T>(this IEnumerable<T> @this, Func<T, int, Task> action)
