5febd134ce816341f12c9be42b1c2f76b3e0e719
jonathan_sumiguin_geeksltd <jonathan.sumiguin@geeks.ltd.uk>
2021-07-19 18:13:42 +0100

    fixed spacings

--- Olive.Mvc/Utilities/FileAccessorFactory.cs
+++ Olive.Mvc/Utilities/FileAccessorFactory.cs
@@ -8,53 +8,53 @@ using Olive.Entities.Data;
 
 namespace Olive.Mvc
 {
-	public class FileAccessorFactory : IFileAccessorFactory
-	{
-		IDatabaseProviderConfig ProviderConfig;
-		readonly IDatabase Database;
+    public class FileAccessorFactory : IFileAccessorFactory
+    {
+        IDatabaseProviderConfig ProviderConfig;
+        readonly IDatabase Database;
 
-		public FileAccessorFactory(IDatabaseProviderConfig providerConfig, IDatabase database)
-		{
-			ProviderConfig = providerConfig;
-			Database = database;
-		}
+        public FileAccessorFactory(IDatabaseProviderConfig providerConfig, IDatabase database)
+        {
+            ProviderConfig = providerConfig;
+            Database = database;
+        }
 
-		public async Task<FileAccessor> Create(string path, IPrincipal currentUser)
-		{
-			var temp = GetTypeAndProperty(path);
+        public async Task<FileAccessor> Create(string path, IPrincipal currentUser)
+        {
+            var temp = GetTypeAndProperty(path);
 
-			var result = new FileAccessor(temp.Type, temp.PropertyInfo, temp.Id, currentUser, Database);
+            var result = new FileAccessor(temp.Type, temp.PropertyInfo, temp.Id, currentUser, Database);
 
-			await result.LoadBlob();
+            await result.LoadBlob();
 
-			return result;
-		}
+            return result;
+        }
 
-		protected virtual (Type Type, PropertyInfo PropertyInfo, string Id) GetTypeAndProperty(string path)
-		{
-			var pathParts = path.Split('/');
+        protected virtual (Type Type, PropertyInfo PropertyInfo, string Id) GetTypeAndProperty(string path)
+        {
+            var pathParts = path.Split('/');
 
-			if (pathParts.Length < 2)
-				throw new Exception($"Invalid path specified: '{path}'");
+            if (pathParts.Length < 2)
+                throw new Exception($"Invalid path specified: '{path}'");
 
-			var typeName = pathParts[0].Split('.')[0];
+            var typeName = pathParts[0].Split('.')[0];
 
-			var type = ProviderConfig.GetRegisteredAssemblies()
-				.Select(a => a.GetExportedTypes().SingleOrDefault(t => t.Name == typeName))
-				.ExceptNull().FirstOrDefault();
+            var type = ProviderConfig.GetRegisteredAssemblies()
+                .Select(a => a.GetExportedTypes().SingleOrDefault(t => t.Name == typeName))
+                .ExceptNull().FirstOrDefault();
 
-			if (type == null)
-				type = AppDomain.CurrentDomain.GetTypeByName(typeof(IEntity), typeName);
+            if (type == null)
+                type = AppDomain.CurrentDomain.GetTypeByName(typeof(IEntity), typeName);
 
-			if (type == null) throw new Exception($"Invalid type name specified: '{typeName}'");
+            if (type == null) throw new Exception($"Invalid type name specified: '{typeName}'");
 
-			var property = pathParts[0].Split('.')[1];
+            var property = pathParts[0].Split('.')[1];
 
-			var propertyInfo = type.GetProperty(property);
-			if (propertyInfo == null)
-				throw new Exception($"Could not find the property '{property}' on the type '{type.FullName}'.");
+            var propertyInfo = type.GetProperty(property);
+            if (propertyInfo == null)
+                throw new Exception($"Could not find the property '{property}' on the type '{type.FullName}'.");
 
-			return (type, propertyInfo, pathParts[1]);
-		}
-	}
+            return (type, propertyInfo, pathParts[1]);
+        }
+    }
 }
