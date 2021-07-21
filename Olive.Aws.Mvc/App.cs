248e8d8f8b4d02f7c7509facdb6d47f20ada84c7
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2021-07-21 14:49:12 +0430

    Initial version of aws.mvc

---
+++ Olive.Aws.Mvc/App.cs
@@ -0,0 +1,34 @@
+﻿using Amazon.Lambda.Core;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace Olive.Aws
+{
+	public abstract class App<TStartup> : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction where TStartup : Startup, new()
+	{
+		protected IConfiguration Configuration { get; private set; }
+
+		protected override void Init(IWebHostBuilder builder)
+		{
+			ConfigureBuilder(builder);
+		}
+
+		public ILogger Log => Olive.Log.For(this);
+
+		protected static void Run(string[] args)
+		{
+			var builder = WebHost.CreateDefaultBuilder(args);
+			ConfigureBuilder(builder);
+			builder.Build().Run();
+		}
+
+		static void ConfigureBuilder(IWebHostBuilder builder)
+		{
+			builder.UseStartup<TStartup>();
+		}
+	}
+}
---
+++ Olive.Aws.Mvc/Startup.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Olive.Aws
+{
+	public abstract class Startup
+	{
+		protected IServiceCollection ServiceCollection { get; private set; }
+		public IServiceProvider Services { get; private set; }
+		public IHostEnvironment Environment { get; private set; }
+		public IConfiguration Configuration { get; private set; }
+		protected IHost Host { get; private set; }
+
+		protected Startup()
+		{
+			CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture =
+				   CultureInfo.CurrentCulture = CultureInfo.CurrentUICulture = GetRequestCulture();
+
+			var host = new HostBuilder();
+			host.ConfigureAppConfiguration((context, builder) =>
+			{
+				Environment = context.HostingEnvironment;
+				builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+				builder.AddEnvironmentVariables();
+				ConfigureConfiguration(context, builder);
+				Configuration = builder.Build();
+			});
+
+			host.ConfigureServices((hostContext, services) =>
+			{
+				ServiceCollection = services;
+				services.AddOptions();
+				Configuration.MergeEnvironmentVariables();
+				ConfigureServices(services);
+				Services = services.BuildServiceProvider();
+				Context.Initialize(Services, null);
+			});
+
+			host.ConfigureLogging((context, logging) =>
+			{
+				logging.AddConfiguration(Configuration.GetSection("Logging"));
+
+				if (Environment.IsDevelopment()) logging.AddConsole();
+				else
+				{
+					logging.AddAWSProvider(Configuration.GetAWSLoggingConfigSection());
+					logging.SetMinimumLevel(LogLevel.Debug);
+				}
+
+				ConfigureLogging(context, logging);
+				Log.Init(Context.Current.GetService<ILoggerFactory>());
+			});
+
+			ConfigureHost(host);
+
+			Host = host.Build();
+		}
+
+		protected virtual CultureInfo GetRequestCulture() => new("en-GB");
+
+		protected virtual void ConfigureHost(HostBuilder hostBuilder) { }
+
+		protected virtual void ConfigureServices(IServiceCollection services)
+		{
+			services.AddCors(c => c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin()));
+			services.AddRazorPages().AddRazorPagesOptions(ConfigureRazorPages);
+		}
+
+		protected virtual void ConfigureLogging(HostBuilderContext context, ILoggingBuilder builder) { }
+
+		protected virtual void ConfigureConfiguration(HostBuilderContext context, IConfigurationBuilder builder) { }
+
+		protected virtual void ConfigureRazorPages(RazorPagesOptions options) { }
+
+		protected string AwsServiceUrl => Configuration["Aws:ServiceUrl"];
+	}
+}
