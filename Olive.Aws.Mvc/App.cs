2f82bc937e6a6af9cb2afced02bcb153b4cb334d
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2021-07-22 12:45:31 +0430

    Add lambda logger

--- Olive.Aws.Mvc/App.cs
+++ Olive.Aws.Mvc/App.cs
@@ -3,32 +3,49 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace Olive.Aws
 {
-	public abstract class App<TStartup> : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction where TStartup : Startup
-	{
-		protected IConfiguration Configuration { get; private set; }
+    public abstract class App<TStartup> : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction where TStartup : Startup
+    {
+        protected IConfiguration Configuration { get; private set; }
 
-		protected override void Init(IWebHostBuilder builder)
-		{
-			ConfigureBuilder(builder);
-		}
+        protected override void Init(IWebHostBuilder builder)
+        {
+            ConfigureBuilder(builder);
+        }
 
-		public ILogger Log => Olive.Log.For(this);
+        public ILogger Log => Olive.Log.For(this);
 
-		protected static void Run(string[] args)
-		{
-			var builder = WebHost.CreateDefaultBuilder(args);
-			ConfigureBuilder(builder);
-			builder.Build().Run();
-		}
+        protected static void Run(string[] args)
+        {
+            var builder = WebHost.CreateDefaultBuilder(args);
+            ConfigureBuilder(builder);
+            builder.Build().Run();
+        }
 
-		static void ConfigureBuilder(IWebHostBuilder builder)
-		{
-			builder.UseStartup<TStartup>();
-		}
-	}
+        static void ConfigureBuilder(IWebHostBuilder builder)
+        {
+            builder
+                .UseStartup<TStartup>()
+                .ConfigureLogging(ConfigureLogging);
+        }
+
+        static void ConfigureLogging(WebHostBuilderContext context, ILoggingBuilder logging)
+        {
+            if (context.HostingEnvironment.IsDevelopment()) return;
+            logging.AddLambdaLogger(new LambdaLoggerOptions
+            {
+                IncludeCategory = true,
+                IncludeScopes = true,
+                IncludeLogLevel = true,
+                IncludeEventId = true,
+                IncludeException = true,
+                IncludeNewline = true,
+            });
+        }
+    }
 }
--- Olive.Aws.Mvc/Startup.cs
+++ Olive.Aws.Mvc/Startup.cs
@@ -6,32 +6,32 @@ using Microsoft.Extensions.Logging;
 
 namespace Olive.Aws
 {
-	public abstract class Startup : Mvc.Startup
-	{
-		protected Startup(IWebHostEnvironment env, IConfiguration config, ILoggerFactory loggerFactory) : base(env, config, loggerFactory)
-		{
-			CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture =
-				   CultureInfo.CurrentCulture = CultureInfo.CurrentUICulture = GetRequestCulture();
-		}
+    public abstract class Startup : Mvc.Startup
+    {
+        protected Startup(IWebHostEnvironment env, IConfiguration config, ILoggerFactory loggerFactory) : base(env, config, loggerFactory)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture =
+                   CultureInfo.CurrentCulture = CultureInfo.CurrentUICulture = GetRequestCulture();
+        }
 
-		protected override CultureInfo GetRequestCulture() => new("en-GB");
+        protected override CultureInfo GetRequestCulture() => new("en-GB");
 
-		public override void ConfigureServices(IServiceCollection services)
-		{
-			base.ConfigureServices(services);
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            base.ConfigureServices(services);
 
-			services.AddCors(c => c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin()));
-		}
+            services.AddCors(c => c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin()));
+        }
 
-		protected override void ConfigureMvc(IMvcBuilder mvc)
-		{
-			base.ConfigureMvc(mvc);
+        protected override void ConfigureMvc(IMvcBuilder mvc)
+        {
+            base.ConfigureMvc(mvc);
 
-			mvc.AddRazorPagesOptions(ConfigureRazorPagesOptions);
-		}
+            mvc.AddRazorPagesOptions(ConfigureRazorPagesOptions);
+        }
 
-		protected virtual void ConfigureRazorPagesOptions(Microsoft.AspNetCore.Mvc.RazorPages.RazorPagesOptions options) { }
+        protected virtual void ConfigureRazorPagesOptions(Microsoft.AspNetCore.Mvc.RazorPages.RazorPagesOptions options) { }
 
-		protected string AwsServiceUrl => Configuration["Aws:ServiceUrl"];
-	}
+        protected string AwsServiceUrl => Configuration["Aws:ServiceUrl"];
+    }
 }
