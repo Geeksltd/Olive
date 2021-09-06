4beabadf50c406a8a534187911f97f808b5777ce
Paymon Khamooshi <paymon@geeks.ltd.uk>
2021-09-06 18:19:06 +0100

    no message

--- Olive.Entities/Attributes/LogEventsAttribute.cs
+++ Olive.Entities/Attributes/LogEventsAttribute.cs
@@ -9,23 +9,57 @@ namespace Olive.Entities
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Property)]
     public sealed class LogEventsAttribute : Attribute
     {
-        const bool DEFAULT_UNCONFIGURED = true;
+        static LogEventsAttribute @default;
+        static LogEventsAttribute Default => @default ??= LoadDefault();
 
-        public bool Log { get; private set; }
+        public bool InsertAction { get; }
+        public bool InsertData { get; }
+        public bool UpdateAction { get; }
+        public bool UpdateData { get; }
+        public bool DeleteAction { get; }
+        public bool DeleteData { get; }
 
         /// <summary>
         /// Creates a new LogEventsAttribute instance.
         /// </summary>
-        public LogEventsAttribute(bool shouldLog) => Log = shouldLog;
+        public LogEventsAttribute(bool shouldLog) : this(shouldLog, shouldLog, shouldLog, shouldLog, shouldLog, shouldLog) { }
 
-        public static bool ShouldLog(Type type)
+        /// <summary>
+        /// Creates a new LogEventsAttribute instance.
+        /// </summary>
+        public LogEventsAttribute(bool insertAction = true,
+            bool insertData = true,
+            bool updateAction = true,
+            bool updateData = true,
+            bool deleteAction = true,
+            bool deleteData = true)
         {
-            var definedAttribute = type.GetCustomAttribute<LogEventsAttribute>(inherit: true);
+            InsertAction = insertAction;
+            InsertData = insertData;
+            UpdateAction = updateAction;
+            UpdateData = updateData;
+            DeleteAction = deleteAction;
+            DeleteData = deleteData;
+        }
 
-            return definedAttribute?.Log ?? DEFAULT_UNCONFIGURED;
+        static bool Setting(string @event, string info) => Config.Get($"Database:Audit:{@event}:{info}", defaultValue: false);
+
+        static LogEventsAttribute LoadDefault()
+        {
+            return new LogEventsAttribute(
+                Setting("Insert", "Action"), Setting("Insert", "Data"),
+                Setting("Update", "Action"), Setting("Update", "Data"),
+                Setting("Delete", "Action"), Setting("Delete", "Data"));
+        }
+
+        public static LogEventsAttribute For(IEntity instance) => For(instance.GetType());
+
+        public static LogEventsAttribute For(Type type)
+        {
+            return type.GetCustomAttribute<LogEventsAttribute>(inherit: true) ?? Default;
         }
 
         public static bool ShouldLog(PropertyInfo property) =>
-            property.GetCustomAttribute<LogEventsAttribute>(inherit: true)?.Log ?? true;
+            property.GetCustomAttribute<LogEventsAttribute>(inherit: true)?.InsertAction /*Does not matter which property*/ ?? true;
     }
 }
