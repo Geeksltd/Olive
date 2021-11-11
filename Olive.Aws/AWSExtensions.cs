fec3f00fcc8691c5cb4b77240ae5a38d4adbc3ca
Paymon Khamooshi <paymon@geeks.ltd.uk>
2021-11-11 14:32:56 +0000

    Aws identity clean up

--- Olive.Aws/AWSExtensions.cs
+++ Olive.Aws/AWSExtensions.cs
@@ -9,12 +9,18 @@ namespace Olive
 {
     public static class AWSExtensions
     {
+        [Obsolete("This is intended for use in Production under Kubernetes pods as a workaround for adding AWS Roles to specific pods as opposed to the host server." +
+            "This is not needed in Lambda as you can allocate a role to lambda directly.")]
         public static void LoadAwsIdentity(this IConfiguration @this)
         {
             RuntimeIdentity.Load(@this).WaitAndThrow();
             @this.LoadAwsSecrets();
         }
 
+        /// <summary>
+        /// Use this if you want to have AWS calls made under the current machine's Role, 
+        /// or the user specified in appSettings under (Aws { Credentials { AccessKey: ... , Secret: ... } }).
+        /// </summary>
         public static void LoadAwsDevIdentity(this IConfiguration @this, bool loadSecrets = false)
         {
             var accessKey = @this["Aws:Credentials:AccessKey"];
@@ -24,15 +30,27 @@ namespace Olive
             @this.LoadAwsDevIdentity(accessKey, secret, endpoint, loadSecrets);
         }
 
+
+        [Obsolete("Instead of hardcoding accessKey and secret, either use the default host credentials (role or default user) " +
+            "or at least use appSettings (under Aws { Credentials { AccessKey: ... , Secret: ... } }).")]
         /// <summary>
-        /// Use this if you want to temporarily simulate production environment access for debugging.
-        /// The accessKey and secret are usually that of a root admin user.
+        /// Use this if you want to have AWS calls made under a user.
         /// </summary>
         public static void LoadAwsDevIdentity(this IConfiguration @this, string accessKey, string secret, RegionEndpoint endpoint, bool loadSecrets)
         {
             AWSConfigs.RegionEndpoint = endpoint;
-            FallbackCredentialsFactory.Reset();
-            FallbackCredentialsFactory.CredentialsGenerators.Insert(0, () => new BasicAWSCredentials(accessKey, secret));
+            if (accessKey.HasValue() && secret.HasValue())
+            {
+                FallbackCredentialsFactory.Reset();
+                FallbackCredentialsFactory.CredentialsGenerators.Insert(0, () => new BasicAWSCredentials(accessKey, secret));
+            }
+            else
+            {
+                Log.For(typeof(AWSExtensions))
+                    .Warning("Using the machine default AWS Role since there is no secret in appSettings under" +
+                     "Aws { Credentials { AccessKey: ... , Secret: ... } }");
+            }
+
             if (loadSecrets) @this.LoadAwsSecrets();
         }
 
