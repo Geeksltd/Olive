2b6c1d3b482ce16b6d9f8957c5d1b9587d24049a
Matt <matt@geeks.ltd.uk>
2022-01-08 13:38:39 +0000

    update

--- Olive.EventBus/EventBus.cs
+++ Olive.EventBus/EventBus.cs
@@ -15,7 +15,10 @@
         /// </summary>
         public static EventBusQueue<TMessage> Queue<TMessage>() where TMessage : IEventBusMessage
         {
-            var url = Config.Get($"EventBus:Queues:{typeof(TMessage).FullName.Replace(".", "_")}:Url").Or(Config.GetOrThrow($"EventBus:Queues:{typeof(TMessage).FullName}:Url"));
+            var url = Config.Get($"EventBus:Queues:{typeof(TMessage).FullName.Replace(".", "_")}:Url");
+            
+            if(url.IsEmpty())
+             url = Config.GetOrThrow($"EventBus:Queues:{typeof(TMessage).FullName}:Url");
             
             return new EventBusQueue<TMessage>(Queue(url));
         }
