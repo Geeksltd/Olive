61b7e2a2703f05993f6f45cf6b32ac4477483b52
Paymon Khamooshi <paymon@geeks.ltd.uk>
2022-01-28 15:35:25 +0000

    AssumeRole

---
+++ Olive.Aws/AssumeRole.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.Runtime;
+using Amazon.SecurityToken;
+using Amazon.SecurityToken.Model;
+using Microsoft.Extensions.Logging;
+
+namespace Olive.Aws
+{
+    public static class AssumeRole
+    {
+        static Task RenewingTask;
+        static ILogger Log => Olive.Log.For(typeof(AssumeRole));
+        static AmazonSecurityTokenServiceClient TokenService = new AmazonSecurityTokenServiceClient();
+        static DateTime LastRenewedUtc;
+        static string RoleArn;
+
+        public static Task Assume(string roleArn)
+        {
+            RoleArn = roleArn;
+            return Renew();
+        }
+
+        public static async Task SignalRenew()
+        {
+            if (LocalTime.UtcNow.Subtract(LastRenewedUtc) > 5.Minutes())
+            {
+                try { await Renew(); }
+                catch
+                {
+                    // No logging needed
+                }
+            }
+        }
+
+        [EscapeGCop("This is a background process")]
+        public static void KeepRenewing()
+        {
+            if (RenewingTask != null) return;
+
+            RenewingTask = Task.Factory.StartNew(async () =>
+            {
+                while (true)
+                {
+                    await Task.Delay(5.Minutes());
+
+                    await SignalRenew();
+                }
+            }, TaskCreationOptions.LongRunning);
+        }
+
+        static async Task Renew()
+        {
+            var request = new AssumeRoleRequest
+            {
+                RoleArn = RoleArn,
+                DurationSeconds = (int)12.Hours().TotalSeconds
+            };
+
+            try
+            {
+                var response = await TokenService.AssumeRoleAsync(request);
+                Log.Debug("AssumeRole response code: " + response.HttpStatusCode);
+
+                if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    LastRenewedUtc = LocalTime.UtcNow;
+                    FallbackCredentialsFactory.Reset();
+                    FallbackCredentialsFactory.CredentialsGenerators.Insert(0, () => response.Credentials);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Submitting Assume Role request failed.");
+                throw;
+            }
+        }
+    }
+}
--- Olive.Aws/RuntimeIdentity.cs
+++ Olive.Aws/RuntimeIdentity.cs
@@ -1,13 +1,6 @@
-﻿using Amazon;
-using Amazon.Runtime;
-using Amazon.SecurityToken;
-using Amazon.SecurityToken.Model;
-using Amazon.Util;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Threading;
+﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace Olive.Aws
 {
@@ -20,81 +13,13 @@ namespace Olive.Aws
         const string VARIABLE = "AWS_RUNTIME_ROLE_ARN";
         static string RoleArn;
 
-        static IConfiguration Config;
-        public static AWSCredentials Credentials { get; private set; }
-        static AmazonSecurityTokenServiceClient TokenServiceClient;
-
-        static ILogger Log => Olive.Log.For(typeof(RuntimeIdentity));
-
         public static async Task Load(IConfiguration config)
         {
-            Config = config;
             RoleArn = Environment.GetEnvironmentVariable(VARIABLE);
             Environment.SetEnvironmentVariable(VARIABLE, null);
-            TokenServiceClient = new AmazonSecurityTokenServiceClient();
 
-            Log.Info("Runtime role ARN > " + RoleArn);
-
-            await Renew();
-            new Thread(KeepRenewing).Start();
-        }
-
-        [EscapeGCop("This is a background process")]
-        static async void KeepRenewing()
-        {
-            var interval = Config.GetValue("Aws:Identity:RenewalInterval", 300).Minutes();
-
-            while (true)
-            {
-                await Task.Delay(interval);
-
-                try
-                {
-                    await Renew();
-                }
-                catch (Exception ex)
-                {
-                    Log.Error(ex, "Failed to renew AWS credentials.");
-                    Environment.Exit(-1);
-                }
-            }
-        }
-
-        static async Task Renew()
-        {
-            Log.Info("Requesting AssumeRole: " + RoleArn + "...");
-
-            var request = new AssumeRoleRequest
-            {
-                RoleArn = RoleArn,
-                DurationSeconds = (int)12.Hours().TotalSeconds,
-                ExternalId = "Pod",
-                RoleSessionName = "Pod"
-            };
-
-            try
-            {
-                var response = await TokenServiceClient.AssumeRoleAsync(request);
-                Log.Debug("AssumeRole response code: " + response.HttpStatusCode);
-                var credentials = response.Credentials;
-
-                FallbackCredentialsFactory.Reset();
-
-                FallbackCredentialsFactory.CredentialsGenerators
-                    .Insert(0, () =>
-                {
-                    Log.Debug("Generating credentials => " + credentials.AccessKeyId.Substring(20) + " of total : " + FallbackCredentialsFactory.CredentialsGenerators.Count);
-                    return credentials;
-                }
-                );
-
-                Log.Debug("Obtained assume role credentials." + credentials.AccessKeyId.Substring(20));
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "Submitting Assume Role request failed.");
-                throw;
-            }
+            await AssumeRole.Assume(RoleArn);
+            AssumeRole.KeepRenewing();
         }
     }
 }
