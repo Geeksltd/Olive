9c6c31392dbfd2bde051ec972628c2bb43cfd458
Matt <matt@geeks.ltd.uk>
2022-03-15 10:44:29 +0000

    update

---
+++ Olive.Entities.Data.Replication.QueueUrlProvider/UrlProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Olive.Entities.Data.Replication.QueueUrlProvider
+{
+    public class UrlProvider
+    {
+        public static string GetUrl(Type type)
+        {
+            var url = Config.Get($"DataReplication:{type.FullName.Replace(".", "_")}:Url");
+
+            if (url.IsEmpty())
+                url = Config.Get($"DataReplication:{type.FullName}:Url");
+
+            if (url.IsEmpty())
+                url = $"FOR_DEVELOPMENT_ONLY_DataReplication_{type.FullName}";
+
+            return url;
+        }
+    }
+}
