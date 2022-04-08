83648e780a70f2e315871835004ddecc916ec18d
sepehr_noori_geeksltd <sepehr.noori@geeks.ltd.uk>
2022-04-08 12:27:26 +0100

    addded comments for feature

--- Olive.Mvc.Microservices/resources/Feature.cs
+++ Olive.Mvc.Microservices/resources/Feature.cs
@@ -19,15 +19,47 @@ namespace Olive.Mvc.Microservices
     /// </summary>
     public class Feature
     {
+        /// <summary>
+        /// Folders in Hub navigation seperated by "/" and the name of the feature shown in Hub.
+        /// </summary>
         public string FullPath;
+        /// <summary>
+        /// Icon of the feature shown in hub.
+        /// </summary>
         public string Icon;
+        /// <summary>
+        /// Relative Url of the feature inside the microservice
+        /// </summary>
         public string RelativeUrl;
+        /// <summary>
+        /// Permissions required to access this feature seperated by ", ".
+        /// </summary>
         public string Permissions;
+        /// <summary>
+        /// Description to be shown under the feature in hub view.
+        /// </summary>
         public string Description;
+        /// <summary>
+        /// Refrance for legacy widgets
+        /// </summary>
         public string Refrance;
+        /// <summary>
+        /// badge reletive Url to be shown in hub.
+        /// </summary>
         public string BadgeUrl;
+        /// <summary>
+        /// For legacy board components to be shown on wright 
+        /// </summary>
         public bool ShowOnRight;
+        /// <summary>
+        /// For old microservices to get rendered as Iframes.
+        /// </summary>
         public bool Iframe;
+        /// <summary>
+        /// Detemines the order of features and folders under hub.
+        /// the lower the number the higher the feature.
+        /// default number for thisone is 100.
+        /// </summary>
         public int? Order;
     }
 }
