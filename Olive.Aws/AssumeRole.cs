d305b0481ac0b922f64b521a26ede6103d1d66fe
Paymon <paymon@geeks.ltd.uk>
2022-07-19 14:36:39 +0100

    Assume Role

--- Olive.Aws/AssumeRole.cs
+++ Olive.Aws/AssumeRole.cs
@@ -15,11 +15,19 @@ namespace Olive.Aws
         static DateTime LastRenewedUtc;
         static string RoleArn;
 
+        /// <summary>
+        /// This will change the default application identity to the specified role in the specified AWS account.
+        /// </summary>
         public static Task Assume(string roleArn)
         {
             RoleArn = roleArn;
             return Renew();
         }
+
+        /// <summary>
+        /// This will create the credentials to use for creating new AWS clients with the identify of the specified role.
+        /// </summary>
+        public static Task<Credentials> Temporary(string roleArn) => LoginAs(roleArn);
 
         public static async Task SignalRenew()
         {
@@ -46,11 +54,20 @@ namespace Olive.Aws
 
         static async Task Renew()
         {
+            var cred = await LoginAs(RoleArn);
+
+            LastRenewedUtc = LocalTime.UtcNow;
+            FallbackCredentialsFactory.Reset();
+            FallbackCredentialsFactory.CredentialsGenerators.Insert(0, () => cred);
+        }
+
+        static async Task<Credentials> LoginAs(string roleArn)
+        {
             var request = new AssumeRoleRequest
             {
-                RoleArn = RoleArn,
-                ExternalId = "Pod",
-                RoleSessionName = "Pod"
+                RoleArn = roleArn,
+                ExternalId = "Temporary",
+                RoleSessionName = "Temporary"
             };
 
             try
@@ -60,10 +77,9 @@ namespace Olive.Aws
 
                 if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
                 {
-                    LastRenewedUtc = LocalTime.UtcNow;
-                    FallbackCredentialsFactory.Reset();
-                    FallbackCredentialsFactory.CredentialsGenerators.Insert(0, () => response.Credentials);
+                    return response.Credentials;
                 }
+                throw new Exception("AssumeRole failed: " + response.HttpStatusCode);
             }
             catch (Exception ex)
             {
