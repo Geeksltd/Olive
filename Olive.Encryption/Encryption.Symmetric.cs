bd0b1405942eb98ec5b10a052fd73228becbf1ad
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2022-09-30 11:07:46 +0330

    Release

--- Olive.Encryption/Encryption.Symmetric.cs
+++ Olive.Encryption/Encryption.Symmetric.cs
@@ -10,7 +10,7 @@ namespace Olive.Security
     {
         const int AES_KEY_LENGTH = 32, AES_IV_LENGTH = 16;
 
-        static byte[] GetValidAesKey(string keyString,int aesLeyLength)
+        static byte[] GetValidAesKey(string keyString, int aesLeyLength)
         {
             var key = Encoding.UTF8.GetBytes(keyString);
             if (key.Length == aesLeyLength) return key;
@@ -19,7 +19,7 @@ namespace Olive.Security
 
         /// <summary>Encrypts the specified text with the specified password.</summary>
         /// <param name="encoding">If not specified, UTF8 will be used.</param>
-        public static string Encrypt(string raw, string password, Encoding encoding = null) => Encrypt(raw,password, AES_KEY_LENGTH, AES_IV_LENGTH,encoding);
+        public static string Encrypt(string raw, string password, Encoding encoding = null) => Encrypt(raw, password, AES_KEY_LENGTH, AES_IV_LENGTH, encoding);
 
         /// <summary>Encrypts the specified text with the specified password.</summary>
         /// <param name="encoding">If not specified, UTF8 will be used.</param>
@@ -28,18 +28,18 @@ namespace Olive.Security
             if (raw.IsEmpty()) throw new ArgumentNullException(nameof(raw));
             if (encoding == null) encoding = Encoding.UTF8;
 
-            return Encrypt(encoding.GetBytes(raw), password,aesKeyLength,aesIvLength).ToBase64String();
+            return Encrypt(encoding.GetBytes(raw), password, aesKeyLength, aesIvLength).ToBase64String();
         }
 
-        public static byte[] Encrypt(byte[] raw, string password) =>Encrypt(raw,password, AES_KEY_LENGTH, AES_IV_LENGTH);
+        public static byte[] Encrypt(byte[] raw, string password) => Encrypt(raw, password, AES_KEY_LENGTH, AES_IV_LENGTH);
 
-        public static byte[] Encrypt(byte[] raw, string password,int aesKeyLength, int aesIvLength)
+        public static byte[] Encrypt(byte[] raw, string password, int aesKeyLength, int aesIvLength)
         {
             if (raw == null) throw new ArgumentNullException(nameof(raw));
 
             using (var aesAlg = Aes.Create())
             {
-                aesAlg.Key = GetValidAesKey(password,aesKeyLength);
+                aesAlg.Key = GetValidAesKey(password, aesKeyLength);
                 aesAlg.IV = aesAlg.Key.Take(aesIvLength).ToArray();
                 var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
 
@@ -56,20 +56,20 @@ namespace Olive.Security
 
         /// <summary>Decrypts the specified text with the specified password.</summary>
         /// <param name="encoding">If not specified, UTF8 will be used.</param>
-        public static string Decrypt(string cipher, string password, Encoding encoding = null)=> Decrypt(cipher, password, AES_KEY_LENGTH, AES_IV_LENGTH, encoding);
+        public static string Decrypt(string cipher, string password, Encoding encoding = null) => Decrypt(cipher, password, AES_KEY_LENGTH, AES_IV_LENGTH, encoding);
 
         /// <summary>Decrypts the specified text with the specified password.</summary>
         /// <param name="encoding">If not specified, UTF8 will be used.</param>
         public static string Decrypt(string cipher, string password, int aesKeyLength, int aesIvLength, Encoding encoding = null)
         {
             if (cipher.IsEmpty()) throw new ArgumentNullException(nameof(cipher));
-            var plainBytes = Decrypt(cipher.ToBytesFromBase64(), password,aesKeyLength,aesIvLength);
+            var plainBytes = Decrypt(cipher.ToBytesFromBase64(), password, aesKeyLength, aesIvLength);
 
             if (encoding == null) encoding = Encoding.UTF8;
             return encoding.GetString(plainBytes);
         }
 
-        public static byte[] Decrypt(byte[] cipher, string password)=>Decrypt(cipher,password, AES_KEY_LENGTH, AES_IV_LENGTH);
+        public static byte[] Decrypt(byte[] cipher, string password) => Decrypt(cipher, password, AES_KEY_LENGTH, AES_IV_LENGTH);
 
         public static byte[] Decrypt(byte[] cipher, string password, int aesKeyLength, int aesIvLength)
         {
