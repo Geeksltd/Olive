67cf01f1d64671901ba995043aae0ed60da57969
atir_naveed_geeksltd <atir.naveed@geeks.ltd.uk>
2022-10-21 16:16:21 +0100

    Update RabbitMQ issue

--- Olive.RabbitMQ/Subscriber.cs
+++ Olive.RabbitMQ/Subscriber.cs
@@ -28,56 +28,95 @@ namespace Olive.RabbitMQ
 
         public Task PullAll() => KeepPolling(PullStrategy.UntilEmpty, waitTimeSeconds: 0);
 
-
-        void Poll(int waitTimeSeconds)
+        BasicGetResult Fetch()
         {
-            var consumer = new EventingBasicConsumer(Queue.Client);
+            try
+            {
+                return Queue.Client.BasicGet(queue: Queue.QueueUrl,
+                                autoAck: false);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to fetch from Queue " + Queue.QueueUrl, ex);
+            }
+        }
 
-            consumer.Received += (model, ea) =>
-               {
-                   try
-                   {
-                       var body = ea.Body.ToArray();
-                       var message = Encoding.UTF8.GetString(body);
-                       Log.For(this)
-                           .Info($"RabbitMQ recieved message: Queue " + Queue.QueueUrl);
-                       Handler(message).WaitAndThrow();
-                       Queue.Client.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
-                   }
-                   catch (Exception ex)
-                   {
-                       var exception = new Exception("Failed to run queue event handler " +
-                           Handler.Method.DeclaringType.GetProgrammingName() + "." +
-                           Handler.Method.Name +
-                           "message: " + ea.DeliveryTag.ToString()?.ToJsonText(), ex);
 
-                       if (Queue.IsFifo)
-                           throw exception;
-                       else
-                           Log.For<Subscriber>().Error(exception);
-                   }
-               };
-            Queue.Client.ExchangeDeclare(exchange: Queue.QueueUrl, type: ExchangeType.Fanout, durable: true);
-            Queue.Client.QueueDeclare(Queue.QueueUrl, true, false, false, null);
-            Queue.Client.QueueBind(queue: Queue.QueueUrl,
-                  exchange: Queue.QueueUrl,
-                  routingKey: Queue.QueueUrl);
-            Queue.Client.BasicConsume(queue: Queue.QueueUrl,
-                                autoAck: false,
-                                consumer: consumer);
+        async Task<bool> Poll(int waitTimeSeconds)
+        {
+            var result = Fetch();
+
+            if (result == null)
+                return false;
+
+            try
+            {
+                var body = result.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                await Handler(message);
+                Queue.Client.BasicAck(deliveryTag: result.DeliveryTag, multiple: false);
+            }
+            catch (Exception ex)
+            {
+                var exception = new Exception("Failed to run queue event handler " +
+                    Handler.Method.DeclaringType.GetProgrammingName() + "." +
+                    Handler.Method.Name +
+                    "message: " + result.DeliveryTag.ToString()?.ToJsonText(), ex);
+
+                if (Queue.IsFifo)
+                    throw exception;
+                else
+                    Log.For<Subscriber>().Error(exception);
+            }
+
+            return true;
+
+            //var consumer = new EventingBasicConsumer(Queue.Client);
+
+            //consumer.Received += (model, ea) =>
+            //   {
+            //       try
+            //       {
+            //           var body = ea.Body.ToArray();
+            //           var message = Encoding.UTF8.GetString(body);
+            //           Log.For(this)
+            //               .Info($"RabbitMQ recieved message: Queue " + Queue.QueueUrl);
+            //           Handler(message).WaitAndThrow();
+            //           Queue.Client.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            //       }
+            //       catch (Exception ex)
+            //       {
+            //           var exception = new Exception("Failed to run queue event handler " +
+            //               Handler.Method.DeclaringType.GetProgrammingName() + "." +
+            //               Handler.Method.Name +
+            //               "message: " + ea.DeliveryTag.ToString()?.ToJsonText(), ex);
+
+            //           if (Queue.IsFifo)
+            //               throw exception;
+            //           else
+            //               Log.For<Subscriber>().Error(exception);
+            //       }
+            //   };
+            //Queue.Client.ExchangeDeclare(exchange: Queue.QueueUrl, type: ExchangeType.Fanout, durable: true);
+            //Queue.Client.QueueDeclare(Queue.QueueUrl, true, false, false, null);
+            //Queue.Client.QueueBind(queue: Queue.QueueUrl,
+            //      exchange: Queue.QueueUrl,
+            //      routingKey: Queue.QueueUrl);
+            //Queue.Client.BasicConsume(queue: Queue.QueueUrl,
+            //                    autoAck: false,
+            //                    consumer: consumer);
+
         }
 
         async Task KeepPolling(PullStrategy strategy = PullStrategy.KeepPulling, int waitTimeSeconds = 10)
         {
-            await Task.Run(() => { });
-            Poll(waitTimeSeconds);
-            //var queueIsEmpty = false;
-            //do
-            //{
-            //try { queueIsEmpty = !await Poll(waitTimeSeconds); }
-            //catch (Exception exception) { Log.For<Subscriber>().Error(exception); }
-            //}
-            //while (strategy == PullStrategy.KeepPulling || !queueIsEmpty);
+            var queueIsEmpty = false;
+            do
+            {
+                try { queueIsEmpty = !await Poll(waitTimeSeconds); }
+                catch (Exception exception) { Log.For<Subscriber>().Error(exception); }
+            }
+            while (strategy == PullStrategy.KeepPulling || !queueIsEmpty);
         }
     }
 }
