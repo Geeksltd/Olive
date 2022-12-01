6036df9d7ec2853d27962e0a6ea2cf49affa41c9
mansour <mansour.nazifiasl@geeks.ltd.uk>
2022-12-01 06:51:04 +0000

    update readme

--- Olive.Aws.Ses.AutoFetch/DatabaseTableService.cs
+++ Olive.Aws.Ses.AutoFetch/DatabaseTableService.cs
@@ -10,26 +10,31 @@ namespace Olive.Aws.Ses.AutoFetch
         const string CREATE_TABLE_COMMAND = @"
 if not exists (select * from sysobjects where name='MailMessages' and xtype='U')
 /****** Object:  Table [dbo].[MailMessages]    ******/
-CREATE TABLE [dbo].[MailMessages](
-	[ID] [uniqueidentifier] NOT NULL,
-	[From] [nvarchar](1000) NOT NULL,
-	[Subject] [nvarchar](1000) NULL,
-	[To] [nvarchar](1000) NULL,
-	[Bcc] [nvarchar](1000) NULL,
-	[Cc] [nvarchar](1000) NULL,
-	[Date] [datetime] NOT NULL,
-	[Sender] [nvarchar](300) NULL,
-	[Body] [nvarchar](max) NULL,
-    [Bucket] [nvarchar](200) NULL,
-) ON [PRIMARY]
+CREATE TABLE [dbo].[MailMessages] (
+    Id uniqueidentifier PRIMARY KEY NONCLUSTERED,
+    [From] nvarchar(2000)  NULL,
+    [To] nvarchar(MAX)  NULL,
+    Bcc nvarchar(2000)  NULL,
+    Cc nvarchar(2000)  NULL,
+    Subject nvarchar(2000)  NULL,
+    Sender nvarchar(2000)  NULL,
+    Body nvarchar(MAX)  NULL,
+    [Date] datetime2  NOT NULL,
+    Processed bit  NOT NULL,
+    Bucket nvarchar(200)  NULL,
+    Attachments nvarchar(MAX)  NULL,
+    DateDownloaded datetime2  NOT NULL,
+    MessageId nvarchar(200)  NULL,
+    ReplyTo nvarchar(200)  NULL
+);
 
 if not exists (select * from sysobjects where name='MailMessageAttachments' and xtype='U')
 /****** Object:  Table [dbo].[MailMessageAttachments]    ******/
-CREATE TABLE [dbo].[MailMessageAttachments](
-	[ID] [uniqueidentifier] NOT NULL,
-	[MailMessage] [uniqueidentifier] NOT NULL,
-	[Attachment_FileName] [nvarchar](1500) NULL
-) ON [PRIMARY]
+CREATE TABLE [dbo].[MailMessageAttachments] (
+    Id uniqueidentifier PRIMARY KEY NONCLUSTERED,
+    MailMessage uniqueidentifier  NULL,
+    Attachment_FileName nvarchar(1500)  NULL
+);
 ";
         internal static Task EnsureDatabaseTable<T,A>() => Entities.Data.DataAccess.Create().ExecuteNonQuery(CREATE_TABLE_COMMAND);
     }
