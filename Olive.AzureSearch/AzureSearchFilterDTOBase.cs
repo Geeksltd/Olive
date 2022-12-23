fcca96708c1e238c0b23fe05b3f0f3334d4ac117
omid_rezaeihanjani_geeksltd <omid.rezaeihanjani@geeks.ltd.uk>
2022-12-23 12:40:18 +0000

    add azure search project
    add a generic repository for search operations
    add an abstract class for DTOs
    add a ConfigModel to be used for setting

---
+++ Olive.AzureSearch/AzureSearchFilterDTOBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Olive.AzureSearch
+{
+    using Azure.Search.Documents.Indexes;
+    using Azure.Search.Documents.Indexes.Models;
+
+    public abstract class AzureSearchFilterDTOBase
+    {
+        [SimpleField(IsKey = true)]
+        public string Id { get; set; }
+    }
+}
