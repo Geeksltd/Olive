80e54ed6780936723c70a90b2bfe6b58abf787fe
Paymon <paymon@geeks.ltd.uk>
2023-01-16 09:48:09 +0000

    SafeRunSync method

--- Olive/-Extensions/Async/RunSync.cs
+++ Olive/-Extensions/Async/RunSync.cs
@@ -17,12 +17,32 @@ namespace Olive
         }
 
         /// <summary>
+        /// Runs a specified task on another thread to remove the likelihood of a deadlock, but waits for it to complete.
+        /// </summary> 
+        [EscapeGCop("I AM the solution to the GCop warning itself!")]
+        public static void RunSyncOnNewThread(this TaskFactory @this, Func<Task> task)
+        {
+            if (task == null) throw new ArgumentNullException(nameof(task));
+
+            @this.StartNew(task, TaskCreationOptions.LongRunning).Unwrap().GetAwaiter().GetResult();
+        }
+
+        /// <summary>
         /// Runs a specified task synchronously.
         /// </summary> 
         [EscapeGCop("I AM the solution to the GCop warning itself!")]
         public static TResult RunSync<TResult>(this TaskFactory @this, Func<Task<TResult>> task)
         {
             return @this.StartNew(task).Unwrap().GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Runs a specified task on another thread to remove the likelihood of a deadlock.
+        /// </summary> 
+        [EscapeGCop("I AM the solution to the GCop warning itself!")]
+        public static TResult RunSyncOnNewThread<TResult>(this TaskFactory @this, Func<Task<TResult>> task)
+        {
+            return @this.StartNew(task, TaskCreationOptions.LongRunning).Unwrap().GetAwaiter().GetResult();
         }
     }
 }
