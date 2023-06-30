a415d8011046648b954e5fecec08cbd8aecfdf52
m-hoseinzadeh <mohammad.hoseinzadehashraf@geeks.ltd.uk>
2023-06-30 14:49:32 +0100

    read bucket name from configurations

--- Olive.Migration/Services/BackupService.cs
+++ Olive.Migration/Services/BackupService.cs
@@ -18,7 +18,8 @@
 
 		public async Task<(bool success, string path, string errorMessage)> Backup(string taskName, WhichBackup whichBackup)
 		{
-			var s3Path = $"arn:aws:s3:::onsuttoninstitutelive-rdsbackups/{_pathService.GetDatabaseName()}/{_pathService.GetBackupFileName(taskName, whichBackup)}";
+			var bucket = Config.Get<string>("DatabaseBackupBucket", "onsuttoninstitutelive-rdsbackups");
+			var s3Path = $"arn:aws:s3:::{bucket}/{_pathService.GetDatabaseName()}/{_pathService.GetBackupFileName(taskName, whichBackup)}";
 			var commandBackup = $"exec msdb.dbo.rds_backup_database @source_db_name='{_pathService.GetDatabaseName()}', @s3_arn_to_backup_to='{s3Path}', @overwrite_S3_backup_file=1;";
 
 			var backupDataTable = await Db.GetAccess().ExecuteQuery(commandBackup);
