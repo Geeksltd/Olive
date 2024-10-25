1797f2467408a5c601ffa27cd9807de03707de10
ehsangfl <ehsan.vali@geeks.ltd.uk>
2024-10-25 19:29:07 +0100

    add V2 assistant suppot

---
+++ Olive.Gpt/AssistantDto/OpenAiCreateAssistantDto.V2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Olive.Gpt.AssistantDto.V2;
+
+public class OpenAiCreateAssistantDto : Olive.Gpt.AssistantDto.OpenAiCreateAssistantDto
+{
+    public new OpenAiAssistantToolsDto[] Tools{ get; set; }
+}
+
+public class OpenAiAssistantToolsDto : Olive.Gpt.AssistantDto.OpenAiAssistantToolsDto
+{
+    [JsonProperty("type")]
+    public new OpenAiAssistantTools Type{ get; set; }
+}
