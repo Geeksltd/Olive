7791478023f33b915f3a588b0e24ba292046c42a
matt_geeksltd <matt@geeks.ltd.uk>
2024-12-19 14:27:39 +0000

    Olive.OpenAI.Voice

---
+++ Olive.OpenAI.Voice/Olive.OpenAI.Voice/OpenAI.cs
@@ -0,0 +1,10 @@
+﻿namespace Olive.OpenAI.Voice
+{
+    public class OpenAI
+    {
+        public async Task<string> ToText(byte[] audio)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
