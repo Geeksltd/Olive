774d979256f658709d1cd183128353b43ee11882
matt_geeksltd <matt@geeks.ltd.uk>
2024-12-19 14:25:04 +0000

    Olive.OpenAI

---
+++ Olive.OpenAI/Olive.OpenAI/OpenAI.cs
@@ -0,0 +1,23 @@
+﻿namespace Olive.OpenAI
+{
+    public class OpenAI
+    {
+        /// <summary>
+        /// Creates a new instance of OpenAI. If no key is passed it reads the OpenAI:Key from config.
+        /// </summary>
+        public OpenAI(string? key = null)
+        {
+            // Get the keys from Config
+        }
+
+        public async Task GetResponse(string[] messages,string instruction= null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async IAsyncEnumerable<string> GetResponseStream(string[] messages, string instruction = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
