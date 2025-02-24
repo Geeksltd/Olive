008464be166446ca3639d5826e586ea3737aad0e
aitazaz_ahsan_geeksltd <aitazaz.ahsan@geeks.ltd.uk>
2025-02-24 14:07:10 +0000

    added olive.mfa

---
+++ Olive.MFA/IMFAManager.cs
@@ -0,0 +1,18 @@
+﻿namespace Olive.MFA
+{
+    using Olive.Email;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IMFAManager
+    {
+        Task SendOtpViaEmail(IEmailMessage emailMessage);
+        Task<bool> SendOtpViaSms(string otp, string phoneNumber);
+        string GetOtp();
+
+
+    }
+}
---
+++ Olive.MFA/MFAExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Olive.MFA
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Olive.SMS;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class MFAExtensions
+    {
+        public static IServiceCollection AddMFA(this IServiceCollection @this)
+        {
+            return @this
+                .AddSingleton<IMFAManager, MFAManager>()
+                 .AddSingleton<ITemporaryLoginService, TemporaryLoginService>();
+        }
+    }
+}
