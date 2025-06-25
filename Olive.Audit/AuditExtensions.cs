6f1c1b62bb54b758d3cf7e947bc9a79b81bb3dde
Paymon Khamooshi <master@geeks.ltd.uk>
2025-06-25 11:02:09 +0100

    Audit log error handling

--- Olive.Audit/AuditExtensions.cs
+++ Olive.Audit/AuditExtensions.cs
@@ -41,41 +41,48 @@ namespace Olive
 
         static async Task<string> ToHtml(this IAuditEvent applicationEvent, string parentNode = null)
         {
-            var item = await EntityProcessor.LoadItem(applicationEvent, parentNode == "new");
-
-            if (item == null)
-                throw new("Could not load the type " + applicationEvent.ItemType);
-
-            var properties = item.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            var ouputBuilder = new StringBuilder();
-
-            if (parentNode == "old")
-                ouputBuilder.AppendLine("<div class =\"audit-log audit-log-old\">");
-            else
-                ouputBuilder.AppendLine("<div class =\"audit-log audit-log-new\">");
-
-            var elements = XElement.Parse(applicationEvent.ItemData).Elements();
-            elements = parentNode.HasValue() ? elements.FirstOrDefault(x => x.Name.LocalName == parentNode)?.Elements() : elements;
-
-            var linker = parentNode == "new" ? "\u2192" : ":";
-
-            foreach (var element in elements.ToArray())
+            try
             {
-                var propertyClass = "audit-log-property";
-                var property = properties.FirstOrDefault(x => x.Name == element.Name.LocalName);
+                var item = await EntityProcessor.LoadItem(applicationEvent, parentNode == "new");
 
-                if (property == null) continue;
+                if (item == null)
+                    throw new("Could not load the type " + applicationEvent.ItemType);
 
-                if (property.IsEntity(item))
-                    propertyClass = $"{propertyClass} audit-log-property-entity";
-                else if (property.PropertyType == typeof(bool))
-                    propertyClass = $"{propertyClass} audit-log-property-bool";
+                var properties = item.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                var ouputBuilder = new StringBuilder();
 
-                ouputBuilder.AppendLine($"<span class=\"audit-log-label\">{element.Name.LocalName?.ToLiteralFromPascalCase()}</span> {linker} <span class=\"{propertyClass}\">{element.Value}</span><br>");
+                if (parentNode == "old")
+                    ouputBuilder.AppendLine("<div class =\"audit-log audit-log-old\">");
+                else
+                    ouputBuilder.AppendLine("<div class =\"audit-log audit-log-new\">");
+
+                var elements = XElement.Parse(applicationEvent.ItemData).Elements();
+                elements = parentNode.HasValue() ? elements.FirstOrDefault(x => x.Name.LocalName == parentNode)?.Elements() : elements;
+
+                var linker = parentNode == "new" ? "\u2192" : ":";
+
+                foreach (var element in elements.ToArray())
+                {
+                    var propertyClass = "audit-log-property";
+                    var property = properties.FirstOrDefault(x => x.Name == element.Name.LocalName);
+
+                    if (property == null) continue;
+
+                    if (property.IsEntity(item))
+                        propertyClass = $"{propertyClass} audit-log-property-entity";
+                    else if (property.PropertyType == typeof(bool))
+                        propertyClass = $"{propertyClass} audit-log-property-bool";
+
+                    ouputBuilder.AppendLine($"<span class=\"audit-log-label\">{element.Name.LocalName?.ToLiteralFromPascalCase()}</span> {linker} <span class=\"{propertyClass}\">{element.Value}</span><br>");
+                }
+
+                ouputBuilder.AppendLine("</div>");
+                return ouputBuilder.ToString();
             }
-
-            ouputBuilder.AppendLine("</div>");
-            return ouputBuilder.ToString();
+            catch (Exception ex)
+            {
+                return "⚠ Failed to load: " + ex.Message;
+            }
         }
 
         public static bool IsEntity(this PropertyInfo property, object obj)
